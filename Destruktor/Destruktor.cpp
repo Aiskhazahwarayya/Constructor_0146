f9228486abd02c8314f612b0388523b0f265c8ab
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-13 14:44:24 +0700

    add Destructor pada class angka

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -20,4 +20,13 @@ angka::angka(int i) {
     isiData();
 }
 
+angka :: ~angka() {
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+
+
 
