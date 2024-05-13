e6101569db16b301ba0671d352e1912d850571f8
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-13 13:56:13 +0700

    add Constructor

---
+++ Constructor/Constructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+public:
+    Mahasiswa() {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim) {
+        nim = iNim;
+    }
+    Mahasiswa(string iNama)
+    {
+        nim = 0;
+        nama = iNama;
+
+    }
+    Mahasiswa(int iNim, string iNama) {
+        nim = iNim;
+        nama = iNama;
+    }
+    void cetak() {
+        cout << endl << "Nim =" << nim << endl;
+        cout << "Nama =" << nama << endl;
+    }
+};
